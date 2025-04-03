1c853d23bffc8f346872659c5d87695dc5a4b29e
Spas Arnaudov <spas.arnaudov@gmail.com>
2025-04-03 15:56:57 +0300

    Made ConfigMaster change data.

--- app/src/main/java/com/spascoding/configmaster/presentation/ConfigScreen.kt
+++ app/src/main/java/com/spascoding/configmaster/presentation/ConfigScreen.kt
@@ -3,52 +3,75 @@ package com.spascoding.configmaster.presentation
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.spascoding.configmaster.domain.models.ConfigEntity
 
 @Composable
 fun ConfigScreen(viewModel: ConfigViewModel = hiltViewModel()) {
-    // Observe the configs LiveData
-    val configs by viewModel.configs.observeAsState(emptyList())
-    val context = LocalContext.current
-    // Fetch configs when the screen is first composed
+    val configs by viewModel.config.observeAsState(emptyList())
     LaunchedEffect(Unit) {
         viewModel.fetchConfigs()
     }
 
     Column {
         Spacer(modifier = Modifier.size(32.dp))
+        Button(
+            onClick = {
+                viewModel.saveConfig()
+            }
+        ) {
+            Text("Save")
+        }
         LazyColumn(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize().padding(8.dp)
         ) {
             items(configs) { config ->
-                ConfigRow(config)
+                ConfigRow(
+                    config = config,
+                    onModifiedValueChanged = { newValue ->
+                        viewModel.updateModifiedValue(config, newValue)
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun ConfigRow(config: ConfigEntity) {
-    Column(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
-        Text("Key: ${config.key}", style = MaterialTheme.typography.bodyLarge)
-        Text("Original: ${config.originalValue}", style = MaterialTheme.typography.bodyMedium)
-        Text("Modified: ${config.modifiedValue}", style = MaterialTheme.typography.bodyMedium)
-        Divider()
-    }
+fun ConfigRow(
+    config: ConfigEntity,
+    onModifiedValueChanged: (String) -> Unit
+) {
+    Text("Key: ${config.key}", style = MaterialTheme.typography.bodyLarge)
+    Text("Original: ${config.originalValue}", style = MaterialTheme.typography.bodyMedium)
+    var modifiedValue by remember { mutableStateOf(config.modifiedValue) }
+    TextField(
+        value = modifiedValue,
+        onValueChange = { newValue ->
+            modifiedValue = newValue
+            onModifiedValueChanged(newValue) // Trigger save to database
+        },
+        label = { Text("Modified Value") },
+        modifier = Modifier.width(200.dp)
+    )
+    Divider()
 }
--- app/src/main/java/com/spascoding/configmaster/presentation/ConfigViewModel.kt
+++ app/src/main/java/com/spascoding/configmaster/presentation/ConfigViewModel.kt
@@ -6,22 +6,37 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.spascoding.configmaster.domain.models.ConfigEntity
 import com.spascoding.configmaster.domain.usecases.GetConfigUseCase
+import com.spascoding.configmaster.domain.usecases.InsertConfigUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ConfigViewModel @Inject constructor(
-    private val getConfigUseCase: GetConfigUseCase
+    private val insertConfigUseCase: InsertConfigUseCase,
+    private val getConfigUseCase: GetConfigUseCase,
 ) : ViewModel() {
 
-    private val _configs = MutableLiveData<List<ConfigEntity>>()
-    val configs: LiveData<List<ConfigEntity>> = _configs
+    private val _config = MutableLiveData<List<ConfigEntity>>()
+    val config: LiveData<List<ConfigEntity>> = _config
 
     fun fetchConfigs() {
         viewModelScope.launch {
             val configList = getConfigUseCase.execute("DemoApp1")
-            _configs.postValue(configList)
+            _config.postValue(configList)
+        }
+    }
+
+    fun updateModifiedValue(config: ConfigEntity, newValue: String) {
+        val updatedConfig = config.copy(modifiedValue = newValue)
+        _config.value = _config.value?.map {
+            if (it.appId == config.appId && it.key == config.key) updatedConfig else it
+        }
+    }
+
+    fun saveConfig() {
+        viewModelScope.launch {
+            _config.value?.let { insertConfigUseCase.execute(it) }
         }
     }
 
