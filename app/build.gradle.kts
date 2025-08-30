edc22a22288466eea42d56e0474a5ec487da093f
Spas Arnaudov <spasarnaudov@gmail.com>
2025-08-30 14:56:38 +0300

    Build version 0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 2
-        versionName = "1.0.${versionCode}"
+        versionName = "0.0.${versionCode}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
