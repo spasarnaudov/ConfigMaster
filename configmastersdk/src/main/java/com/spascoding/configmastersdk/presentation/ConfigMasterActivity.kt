ed5d539e9b6a89a02a75ac2030ab898c1ac66bf8
Spas Arnaudov <spasarnaudov@gmail.com>
2025-09-28 09:48:19 +0300

    Fixed ConfigMasterActivity.kt

--- configmastersdk/src/main/java/com/spascoding/configmastersdk/presentation/ConfigMasterActivity.kt
+++ configmastersdk/src/main/java/com/spascoding/configmastersdk/presentation/ConfigMasterActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.spascoding.configmastersdk.presentation.theme.ConfigMasterTheme
@@ -18,7 +20,13 @@ class ConfigMasterActivity : ComponentActivity() {
         setContent {
             ConfigMasterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ConfigScreen()
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        ConfigScreen()
+                    }
                 }
             }
         }
